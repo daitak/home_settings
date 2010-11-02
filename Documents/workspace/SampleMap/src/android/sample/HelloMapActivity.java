73603ad1bfa8c8e3ab6aed6fdd2dfdb142312758
takasao <takasao@192.168.0.52>
2010-11-02 14:00:01 +0900

    sample commit

---
+++ Documents/workspace/SampleMap/src/android/sample/HelloMapActivity.java
@@ -0,0 +1,20 @@
+package android.sample;
+
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+
+public class HelloMapActivity extends MapActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+	@Override
+	protected boolean isRouteDisplayed(){
+		return false;
+	}
+
+}
